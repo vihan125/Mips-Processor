624e9424a587907f28678af54f08c9fb239d1944
vihan melaka <vihan97@gmail.com>
2021-10-15 15:16:13 +0530

    JR control module added
    module to give output for JR control added

--- Mips-processor/Mips-processor.sim/sim_1/behav/elaborate.bat
+++ Mips-processor/Mips-processor.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
-call %xv_path%/xelab  -wto ce9ec7a66d2749a2bac21839005f6abd -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot Shift_left2_TB_behav xil_defaultlib.Shift_left2_TB -log elaborate.log
+call %xv_path%/xelab  -wto ce9ec7a66d2749a2bac21839005f6abd -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot JR_control_unit_TB_behav xil_defaultlib.JR_control_unit_TB -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
--- Mips-processor/Mips-processor.sim/sim_1/behav/simulate.bat
+++ Mips-processor/Mips-processor.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
-call %xv_path%/xsim Shift_left2_TB_behav -key {Behavioral:sim_1:Functional:Shift_left2_TB} -tclbatch Shift_left2_TB.tcl -log simulate.log
+call %xv_path%/xsim JR_control_unit_TB_behav -key {Behavioral:sim_1:Functional:JR_control_unit_TB} -tclbatch JR_control_unit_TB.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
