9c541c65510f8a5147d11a62ff07ad15dc6ff524
vihan melaka <vihan97@gmail.com>
2021-10-22 16:18:35 +0530

    3 X 32 X 32 MUX
    multiplexer to choose a single 32 bus from 3 busses.
    00 - 1 bus
    10 - 2 bus
    01 - 3 bus

--- Mips-processor/Mips-processor.sim/sim_1/behav/simulate.bat
+++ Mips-processor/Mips-processor.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.3\\bin
-call %xv_path%/xsim Discard_Inst_TB_behav -key {Behavioral:sim_1:Functional:Discard_Inst_TB} -tclbatch Discard_Inst_TB.tcl -log simulate.log
+call %xv_path%/xsim Forwarding_Mux_TB_behav -key {Behavioral:sim_1:Functional:Forwarding_Mux_TB} -tclbatch Forwarding_Mux_TB.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
